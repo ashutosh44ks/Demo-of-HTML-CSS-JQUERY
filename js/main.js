6c904f6c124aa65f761c616454d5597f9b4d1e11
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2020-09-15 20:23:21 +0530

    Add project files
    3 separate folders for css, javascript, and media files and the index page are added.

---
+++ js/main.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+
+  function underdev(){
+    alert("Sorry! This option is currently under development.");
+  }
+  function printThis(){
+    if($(this).text()){
+      console.log($(this).text());
+    }
+    else{
+      console.log($(this).val());
+    }
+  }
+
+
+  $(document).on('contextmenu', function(){ //deactivating default right click function for the whole document
+    return false;
+    //event.preventDefault(); // we can also use - return false;
+  })
+
+  $(document).on('mousedown', function(event){
+    event.stopPropagation();
+    if(event.which==3){ //if right clicked
+      $('.hidden').removeClass('shown');
+      if($(event.target).attr('class')=='pics'){
+        $('.save-img, .new-tab').addClass('shown');
+      }
+      else if($(event.target).is('a')){
+        $('.new-tab').addClass('shown');
+      }
+      $('#context').css({ //change css of it to
+        top: event.pageY,
+        left: event.pageX
+      });
+      $('#context').fadeIn();
+      return false; //let's it stay after fading in
+    }
+    $('#context').fadeOut();
+  })
+
+
+
+  $('[data-trigger="dropdown"]').on('mouseenter', function(){
+    let submenu = $(this).parent().find('.sub-menu'); //.parent() - Get the parent of each element in the current set of matched elements, .find() - Get the descendants of each element in the current set of matched elements
+    submenu.fadeIn(300); //.fadeIn - Display the matched elements by fading them to opaque.
+    $('.profile-menu').on('mouseleave', function(){
+      submenu.fadeOut(300); //.fadeOut - Hide the matched elements by fading them to transparent.
+    })
+  });
+
+  $('.sub-menu-items').on('click', underdev);
+
+  $('.theme').on('click', function(){
+    if($('#style').attr('href')=="css/style.css"){
+      $('#style').attr('href', "css/dark.css");
+      $('.theme').html("Light Mode");
+    }
+    else{
+      $('#style').attr('href', "css/style.css");
+      $('.theme').html("Dark Mode");
+    }
+  })
+
+  $('.log').on('click', function(){
+    if($('.menu-trigger').html()==' Signed In '){
+      alert('Logged Out');
+      $('.log').html("Log In");
+      $('.menu-trigger').html("Signed Out");
+    }
+    else{
+      alert('Logged In');
+      $('.log').html("Log Out");
+      $('.menu-trigger').html("Signed In");
+    }
+  })
+
+
+
+  $('#search-button').on('click', function(){ //.on - Attach an event handler function for one or more events to the selected elements.
+    alert("Sorry! Search Bar is currently under development. \nYou won't see this message again.");
+    $('#search-button').html("Error!"); //.html - Get the HTML contents of the first element in the set of matched elements or set the HTML contents of every matched element.
+    $('#search-button').off('click'); //off - Remove an event handler.
+    $('.my-input').val("");
+  });
+
+
+
+  $('input').focusout(printThis);
+  $('.mail-box').focusout(function(){
+    if(!($(this).val().indexOf('@')>-1) && !($(this).val().indexOf('.')>-1)){
+      alert('INVALID ID');
+    }
+  })
+
+  $('#submit').on('click', function(){
+    alert('Thank you for joining our subscription service!');
+    $('.mail-box').val("");
+  })
+
+})
